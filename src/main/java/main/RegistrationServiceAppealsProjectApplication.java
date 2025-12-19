44bdaae26d2027497dc72c6afec4afd29bd4ba79
pepega <werdewrs555@gmail.com>
2025-12-19 21:03:53 +0300

    feature(configuration) : Added postgresql and redis configuration

--- src/main/java/main/RegistrationServiceAppealsProjectApplication.java
+++ src/main/java/main/RegistrationServiceAppealsProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegistrationSerivceAppealsProjectApplication {
+public class RegistrationServiceAppealsProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RegistrationSerivceAppealsProjectApplication.class, args);
+        SpringApplication.run(RegistrationServiceAppealsProjectApplication.class, args);
     }
 
 }
