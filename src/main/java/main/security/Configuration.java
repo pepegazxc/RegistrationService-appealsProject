2de8b5bde76bfc303291f15dba9dd453537bbfe4
pepega <werdewrs555@gmail.com>
2025-12-20 22:12:13 +0300

    feature(secuirty) : Added secuirty configuration template

---
+++ src/main/java/main/security/Configuration.java
@@ -0,0 +1,23 @@
+package main.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@org.springframework.context.annotation.Configuration
+@EnableWebSecurity
+public class Configuration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
