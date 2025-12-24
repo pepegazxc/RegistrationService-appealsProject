3a697fa95d52da68e36ff169f4da93af4f7c9b8a
pepega <werdewrs555@gmail.com>
2025-12-24 23:19:15 +0300

    feature(service) : Added initial registration logic

--- src/main/java/main/entity/UsersEntity.java
+++ src/main/java/main/entity/UsersEntity.java
@@ -3,6 +3,8 @@ package main.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
 
@@ -31,10 +33,54 @@ public class UsersEntity {
     @Column(name = "hash_password")
     String hashPassword;
 
+    @CreationTimestamp
     @Column(name = "created_at")
     Timestamp createdAt;
 
+    @UpdateTimestamp
     @Column(name = "updated_at")
     Timestamp updatedAt;
 
+    public static class Builder{
+        UsersEntity users = new UsersEntity();
+
+        public Builder name(String name){
+            users.name = name;
+            return this;
+        }
+
+        public Builder surname(String surname){
+            users.surname = surname;
+            return this;
+        }
+
+        public Builder userIdentifier(String userIdentifier){
+            users.userIdentifier = userIdentifier;
+            return this;
+        }
+
+        public Builder cipherEmail(String cipherEmail){
+            users.cipherEmail = cipherEmail;
+            return this;
+        }
+
+        public Builder cipherPhoneNumber(String cipherPhoneNumber){
+            users.cipherPhoneNumber = cipherPhoneNumber;
+            return this;
+        }
+
+        public Builder hashPassword(String hashPassword){
+            users.hashPassword = hashPassword;
+            return this;
+        }
+
+        public Builder createdAt(Timestamp createdAt){
+            users.createdAt = createdAt;
+            return this;
+        }
+
+        public UsersEntity build(){
+            return users;
+        }
+    }
 }
--- src/main/java/main/service/RegistrationService.java
+++ src/main/java/main/service/RegistrationService.java
@@ -1,14 +1,31 @@
 package main.service;
 
 import main.dto.UserRequest;
+import main.entity.UsersEntity;
+import main.repository.RegistrationUserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
 
 @Service
 public class RegistrationService {
 
+    private final RegistrationUserRepository registrationRepository;
+
+    public RegistrationService(RegistrationUserRepository registrationRepository) {
+        this.registrationRepository = registrationRepository;
+    }
+
     @Transactional
     public void registration(UserRequest request){
-
+        UsersEntity user = new UsersEntity.Builder()
+                .name(request.getName())
+                .surname(request.getSurname())
+                .userIdentifier(null)
+                .cipherEmail(request.getEmail())
+                .cipherPhoneNumber(request.getPhoneNumber())
+                .hashPassword(request.getPassword())
+                .build();
     }
 }
