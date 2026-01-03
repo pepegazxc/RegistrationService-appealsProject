6e5c67f87019a7e843ce24cbeddbc2d2b7f478f2
pepega <werdewrs555@gmail.com>
2026-01-03 17:27:51 +0300

    feature(entity) : Added UserDetails to entity class

--- src/main/java/main/entity/UsersEntity.java
+++ src/main/java/main/entity/UsersEntity.java
@@ -5,14 +5,20 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class UsersEntity {
+public class UsersEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -40,6 +46,41 @@ public class UsersEntity {
     @UpdateTimestamp
     @Column(name = "updated_at")
     Timestamp updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return hashPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
     public static class Builder{
         UsersEntity users = new UsersEntity();
