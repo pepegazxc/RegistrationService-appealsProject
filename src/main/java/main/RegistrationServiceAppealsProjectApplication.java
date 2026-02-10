e7754bceab2cd04a1cf7e01418453245b8268c19
pepegazxc <werdewrs555@gmail.com>
2026-02-10 17:43:50 +0300

    feature(sessions) : Added the ability to store user sesstions in Redis

--- src/main/java/main/RegistrationServiceAppealsProjectApplication.java
+++ src/main/java/main/RegistrationServiceAppealsProjectApplication.java
@@ -2,7 +2,9 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 604800)
 @SpringBootApplication
 public class RegistrationServiceAppealsProjectApplication {
 
---
+++ src/main/java/main/configuration/RedisConfig.java
@@ -0,0 +1,38 @@
+package main.configuration;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.protocol.ProtocolVersion;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+        redisConfig.setPassword(password);
+        redisConfig.setUsername("default");
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientOptions(ClientOptions.builder()
+                        .protocolVersion(ProtocolVersion.RESP2)
+                        .build())
+                .build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
+}
