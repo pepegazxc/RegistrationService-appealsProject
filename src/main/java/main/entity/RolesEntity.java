ccfd06984d89afdbe4ec32f805b82c1141008481
pepegazxc <werdewrs555@gmail.com>
2026-03-02 16:03:58 +0300

    feature(entity) : Add roles entity

---
+++ src/main/java/main/entity/RolesEntity.java
@@ -0,0 +1,25 @@
+package main.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RolesEntity {
+    @Id
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+}
