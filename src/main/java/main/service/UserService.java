c408e13ed10751669e098b09e0831a0bc9d2d264
pepegazxc <werdewrs555@gmail.com>
2026-03-05 15:34:03 +0300

    fix(service) : Fix a bug related to role assignment

--- src/main/java/main/service/UserService.java
+++ src/main/java/main/service/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.management.relation.Role;
 import java.util.Optional;
 
 @Service
@@ -52,8 +53,8 @@ public class UserService implements UserDetailsService {
     @Transactional
     public void registration(UserRequest request){
 
-        RolesEntity role = rolesRepository.findByRoleName("ROLE_" + request.getRole().toString().toUpperCase())
-                .orElseThrow(() -> new RuntimeException("Role not found"));
+        RolesEntity role = findRole(request)
+                .orElseThrow(() -> new IllegalStateException("Role not found"));
 
         UsersEntity user = addNewUser(request, role);
 
@@ -90,7 +91,7 @@ public class UserService implements UserDetailsService {
     }
 
     private Optional<RolesEntity> findRole(UserRequest request){
-        return rolesRepository.findByRoleName("ROLE_" + request.getRole().toString().toUpperCase());
+        return rolesRepository.findByRoleName(request.getRole().toString().trim().toUpperCase());
     }
 
 }
