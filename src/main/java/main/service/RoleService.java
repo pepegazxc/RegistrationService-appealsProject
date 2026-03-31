465e910c170b7350c279f31ad0fbe8066839addb
pepegazxc <werdewrs555@gmail.com>
2026-03-31 13:38:57 +0300

    refactor(service) : Add role service

---
+++ src/main/java/main/service/RoleService.java
@@ -0,0 +1,21 @@
+package main.service;
+
+import main.entity.RolesEntity;
+import main.exception.user.RoleNotFoundException;
+import main.repository.RolesRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    private final RolesRepository rolesRepository;
+
+    public RoleService(RolesRepository rolesRepository) {
+        this.rolesRepository = rolesRepository;
+    }
+
+    public RolesEntity findRole(String role) {
+        return rolesRepository.findByRoleName(role.trim().toUpperCase())
+                .orElseThrow(() -> new RoleNotFoundException());
+    }
+}
