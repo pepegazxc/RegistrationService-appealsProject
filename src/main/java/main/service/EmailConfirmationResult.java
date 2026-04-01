9c439bdb5081336d1a801a6034ee449f84317367
pepegazxc <werdewrs555@gmail.com>
2026-04-01 13:27:40 +0300

    feature(service) : Add response service

---
+++ src/main/java/main/service/EmailConfirmationResult.java
@@ -0,0 +1,36 @@
+package main.service;
+
+import main.dto.response.EmailConfirmResultResponse;
+import main.entity.UsersEntity;
+import main.service.jwt.AuthTokenService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmailConfirmationResult {
+
+    private final AuthTokenService jwt;
+    private final EmailVerificationService email;
+    private final AdminRequestService adminRequestService;
+
+    public EmailConfirmationResult(AuthTokenService jwt, EmailVerificationService email, AdminRequestService adminRequestService) {
+        this.jwt = jwt;
+        this.email = email;
+        this.adminRequestService = adminRequestService;
+    }
+
+    @Transactional
+    public EmailConfirmResultResponse confirmationResult(String token) {
+        UsersEntity user = email.confirmUserEmail(token);
+
+        if (user.getRole().getRoleName().equals("USER")) {
+            String jwtToken = jwt.generateJwtTokenForCurrentUser();
+
+            return EmailConfirmResultResponse.userEmailConfirmed(jwtToken);
+        }
+
+        adminRequestService.addAdminRequest(user);
+
+        return EmailConfirmResultResponse.adminEmailConfirmed();
+    }
+}
