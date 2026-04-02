647cd3723cbba3abc9bf8b1276fc3cdbfb33ea1a
pepegazxc <werdewrs555@gmail.com>
2026-04-02 21:27:01 +0300

    feature(service) : Add service for admin request response processing

--- src/main/java/main/event/AdminRequestResponseEvent.java
+++ src/main/java/main/event/AdminRequestResponseEvent.java
@@ -1,13 +1,16 @@
 package main.event;
 
 import lombok.Getter;
+import main.dto.enums.AdminActionEnum;
 
 @Getter
 public class AdminRequestResponseEvent {
 
     private final String email;
+    private final AdminActionEnum action;
 
-    public AdminRequestResponseEvent(String email) {
+    public AdminRequestResponseEvent(String email, AdminActionEnum action) {
         this.email = email;
+        this.action = action;
     }
 }
---
+++ src/main/java/main/service/AdminRequestResponseResultService.java
@@ -0,0 +1,27 @@
+package main.service;
+
+import main.dto.enums.AdminActionEnum;
+import main.service.jwt.AuthTokenService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminRequestResponseResultService {
+
+    private final AuthTokenService jwtService;
+
+    public AdminRequestResponseResultService(AuthTokenService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    public String handleAdminRequestResult(AdminActionEnum action){
+
+        if (action == AdminActionEnum.APPROVED){
+            String jwt = jwtService.generateJwtTokenForCurrentUser();
+            return  "Your request has been approved! Your jwt: " + jwt;
+        }else if (action == AdminActionEnum.REJECTED){
+            return "Your request has been rejected. You also can try again";
+        }
+
+        throw new IllegalStateException();
+    }
+}
