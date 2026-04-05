ca9cf418072e4458acf336f5e58a32951e343c1a
pepegazxc <werdewrs555@gmail.com>
2026-04-05 19:32:31 +0300

    feature(repository) : Add method for check exipred admin requests

--- src/main/java/main/repository/AdminRequestRepository.java
+++ src/main/java/main/repository/AdminRequestRepository.java
@@ -2,9 +2,20 @@ package main.repository;
 
 import main.entity.AdminRequestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface AdminRequestRepository extends JpaRepository<AdminRequestEntity, Long> {
     Optional<AdminRequestEntity> findByToken(String token);
+
+    @Modifying
+    @Query(value ="UPDATE admin_request " +
+            "SET status_id = (SELECT id FROM admin_request_status WHERE status = 'EXPIRED'), " +
+            "reviewed_at = now() " +
+            "WHERE status_id = (SELECT id FROM admin_request_status WHERE status = 'PENDING') " +
+            "AND expires_at < now();"
+            ,nativeQuery = true)
+    int expireOldRequest();
 }
