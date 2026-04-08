c351fc88b82743be7bc2019c46be59a856d85fd5
pepegazxc <werdewrs555@gmail.com>
2026-04-08 11:21:38 +0300

    feature(entity) : Add mayor request entity

---
+++ src/main/java/main/entity/MayorRequestEntity.java
@@ -0,0 +1,39 @@
+package main.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mayor_request")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MayorRequestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UsersEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    private MayorStatusRequestEntity status;
+
+    private String token;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "reviewed_at")
+    private LocalDateTime reviewed_at;
+
+    @Column(name = "is_used")
+    private Boolean isUsed;
+}
