5c41114f88cc78924df7a1a04c7260fe4405460d
pepegazxc <werdewrs555@gmail.com>
2026-04-27 14:07:28 +0300

    feature(test) : Add successful test on email confirmation result service for user role

---
+++ src/test/java/email/EmailConfirmationResultServiceTest.java
@@ -0,0 +1,57 @@
+package email;
+
+import main.dto.response.EmailConfirmResultResponse;
+import main.dto.response.EmailConfirmationResponse;
+import main.entity.RolesEntity;
+import main.entity.UsersEntity;
+import main.service.application.AdminRequestService;
+import main.service.application.EmailConfirmationResultService;
+import main.service.application.EmailVerificationService;
+import main.service.application.MayorRequestService;
+import main.service.infrastructure.jwt.AuthTokenService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailConfirmationResultServiceTest {
+
+    @Mock private AuthTokenService jwt;
+    @Mock private EmailVerificationService email;
+    @Mock private AdminRequestService adminRequestService;
+    @Mock private MayorRequestService mayorRequestService;
+
+    @InjectMocks
+    private EmailConfirmationResultService service;
+
+    @Test
+    public void confirmationResult_UserRole_successful(){
+        String fakeToken = "token";
+        RolesEntity fakeRole = new RolesEntity();
+        fakeRole.setRoleName("USER");
+        UsersEntity fakeUser = new UsersEntity();
+        fakeUser.setRole(fakeRole);
+
+        when(email.confirmUserEmail(fakeToken))
+                .thenReturn(fakeUser);
+
+        when(jwt.generateJwtTokenForCurrentUser())
+                .thenReturn("jwt");
+
+
+        EmailConfirmResultResponse email = service.confirmationResult(fakeToken);
+
+        verify(jwt).generateJwtTokenForCurrentUser();
+        assertEquals("jwt", email.getToken());
+        verifyNoInteractions(adminRequestService, mayorRequestService);
+
+
+    }
+
+}
