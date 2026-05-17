de014a77854b24b2ba5a93341ed3967254175833
pepegazxc <werdewrs555@gmail.com>
2026-05-17 18:32:21 +0300

    refactor(test) : Replace publisher method to kafka

--- src/test/java/registration/RegistrationTest.java
+++ src/test/java/registration/RegistrationTest.java
@@ -4,9 +4,9 @@ import main.dto.enums.RolesEnum;
 import main.dto.request.UserRequest;
 import main.entity.RolesEntity;
 import main.entity.UsersEntity;
-import main.event.RegistrationEvent;
 import main.exception.user.RoleNotFoundException;
 import main.exception.user.UserIdentifierException;
+import main.producer.KafkaProducer;
 import main.repository.UserRepository;
 import main.service.application.EmailVerificationService;
 import main.service.application.RegistrationService;
@@ -21,7 +21,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.mockito.Mockito.*;
@@ -30,14 +29,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 public class RegistrationTest {
 
-
-    @Mock private  UserRepository userRepository;
-    @Mock private  CipherService cipher;
+    @Mock private UserRepository userRepository;
+    @Mock private CipherService cipher;
     @Mock private PasswordEncoder encoder;
-    @Mock private  UserIdentifierService userIdentifier;
-    @Mock private  EmailVerificationService emailService;
-    @Mock private  ApplicationEventPublisher publisher;
-    @Mock private  RoleService roleService;
+    @Mock private UserIdentifierService userIdentifier;
+    @Mock private EmailVerificationService emailService;
+    @Mock private KafkaProducer kafka;
+    @Mock private RoleService roleService;
 
     @InjectMocks
     private RegistrationService registration;
@@ -46,7 +44,7 @@ public class RegistrationTest {
     private RolesEntity fakeRole;
 
     @BeforeEach
-    void globalSetUp(){
+    void globalSetUp() {
         request = new UserRequest();
         request.setName("UserName");
         request.setSurname("UserSurname");
@@ -61,18 +59,18 @@ public class RegistrationTest {
     class SuccessRegistration {
 
         @BeforeEach
-        void successSetUp(){
+        void successSetUp() {
             when(emailService.generateTokenForEmail(any(UsersEntity.class)))
                     .thenReturn("token");
             when(cipher.decrypt(any())).thenReturn("UserEmail@gmail.com");
+            when(cipher.encrypt(any())).thenReturn("encrypted");
             when(userIdentifier.generate()).thenReturn("USR-111");
-            when(encoder.encode((any()))).thenReturn("hash");
+            when(encoder.encode(any())).thenReturn("hash");
         }
 
         @Test
         public void registration_success_shouldRegisterUserWithUserRole() {
             request.setRole(RolesEnum.user);
-
             fakeRole.setRoleName("user");
 
             when(roleService.findRole(request.getRole().toString()))
@@ -81,27 +79,18 @@ public class RegistrationTest {
             registration.registration(request);
 
             ArgumentCaptor<UsersEntity> captorUser = ArgumentCaptor.forClass(UsersEntity.class);
-            ArgumentCaptor<RegistrationEvent> captorEvent = ArgumentCaptor.forClass(RegistrationEvent.class);
-
             verify(userRepository).save(captorUser.capture());
 
-            UsersEntity fakeUser = captorUser.getValue();
-            assertEquals(fakeRole, fakeUser.getRole(), "Roles must be equals");
+            UsersEntity savedUser = captorUser.getValue();
+            assertEquals(fakeRole, savedUser.getRole(), "Roles must be equals");
 
-            verify(publisher).publishEvent(captorEvent.capture());
-
-            RegistrationEvent fakeEvent = captorEvent.getValue();
-            assertNotNull(fakeEvent.getEmail(), "Email must not be empty");
-            assertNotNull(fakeEvent.getToken(), "Token for email confirm must not be empty");
-            assertEquals("token", fakeEvent.getToken(), "Token should match generated one");
-
-            verify(emailService).generateTokenForEmail(fakeUser);
+            verify(emailService).generateTokenForEmail(savedUser);
+            verify(kafka).handleRegistration("UserEmail@gmail.com", "token");
         }
 
         @Test
         public void registration_success_shouldRegisterNewUserWithPendingAdminRole() {
             request.setRole(RolesEnum.admin);
-
             fakeRole.setRoleName("PENDING_ADMIN");
 
             when(roleService.findRole("PENDING_ADMIN"))
@@ -110,31 +99,21 @@ public class RegistrationTest {
             registration.registration(request);
 
             ArgumentCaptor<UsersEntity> captorUser = ArgumentCaptor.forClass(UsersEntity.class);
-            ArgumentCaptor<RegistrationEvent> captorEvent = ArgumentCaptor.forClass(RegistrationEvent.class);
-
             verify(roleService).findRole("PENDING_ADMIN");
             verify(userRepository).save(captorUser.capture());
             verify(userRepository).save(argThat(user ->
                     user.getRole().getRoleName().equals("PENDING_ADMIN")));
 
-            UsersEntity fakeUser = captorUser.getValue();
-            assertEquals(fakeRole, fakeUser.getRole(), "Roles must be equals");
+            UsersEntity savedUser = captorUser.getValue();
+            assertEquals(fakeRole, savedUser.getRole(), "Roles must be equals");
 
-
-            verify(publisher).publishEvent(captorEvent.capture());
-
-            RegistrationEvent fakeEvent = captorEvent.getValue();
-            assertNotNull(fakeEvent.getEmail(), "Email must not be empty");
-            assertNotNull(fakeEvent.getToken(), "Token for email confirm must not be empty");
-            assertEquals("token", fakeEvent.getToken(), "Token should match generated one");
-
-            verify(emailService).generateTokenForEmail(fakeUser);
+            verify(emailService).generateTokenForEmail(savedUser);
+            verify(kafka).handleRegistration("UserEmail@gmail.com", "token");
         }
 
         @Test
         public void registration_success_shouldRegisterNewUserWithPendingMayorRole() {
             request.setRole(RolesEnum.mayor);
-
             fakeRole.setRoleName("PENDING_MAYOR");
 
             when(roleService.findRole("PENDING_MAYOR"))
@@ -143,68 +122,60 @@ public class RegistrationTest {
             registration.registration(request);
 
             ArgumentCaptor<UsersEntity> captorUser = ArgumentCaptor.forClass(UsersEntity.class);
-            ArgumentCaptor<RegistrationEvent> captorEvent = ArgumentCaptor.forClass(RegistrationEvent.class);
-
             verify(roleService).findRole("PENDING_MAYOR");
             verify(userRepository).save(captorUser.capture());
             verify(userRepository).save(argThat(user ->
                     user.getRole().getRoleName().equals("PENDING_MAYOR")));
 
-            UsersEntity fakeUser = captorUser.getValue();
-            assertEquals(fakeRole, fakeUser.getRole(), "Roles must be equals");
+            UsersEntity savedUser = captorUser.getValue();
+            assertEquals(fakeRole, savedUser.getRole(), "Roles must be equals");
 
-
-            verify(publisher).publishEvent(captorEvent.capture());
-
-            RegistrationEvent fakeEvent = captorEvent.getValue();
-            assertNotNull(fakeEvent.getEmail(), "Email must not be empty");
-            assertNotNull(fakeEvent.getToken(), "Token for email confirm must not be empty");
-            assertEquals("token", fakeEvent.getToken(), "Token should match generated one");
-
-            verify(emailService).generateTokenForEmail(fakeUser);
+            verify(emailService).generateTokenForEmail(savedUser);
+            verify(kafka).handleRegistration("UserEmail@gmail.com", "token");
         }
     }
 
     @Nested
-    class FailedRegistration{
+    class FailedRegistration {
 
         @BeforeEach
-        void failedSetUp(){
+        void failedSetUp() {
             request.setRole(RolesEnum.user);
         }
 
         @Test
-        public void registration_failed_shouldReturnRoleNotFoundException(){
+        public void registration_failed_shouldReturnRoleNotFoundException() {
             when(roleService.findRole(anyString())).thenThrow(new RoleNotFoundException());
 
             assertThrows(RoleNotFoundException.class, () ->
                     registration.registration(request));
 
             verify(userRepository, never()).save(any());
+            verifyNoInteractions(kafka);
         }
+
         @Test
-        public void registration_failed_shouldReturnUserIdentifierException(){
+        public void registration_failed_shouldReturnUserIdentifierException() {
             when(userIdentifier.generate()).thenThrow(new UserIdentifierException());
 
             assertThrows(UserIdentifierException.class, () ->
                     registration.registration(request));
 
             verify(userRepository, never()).save(any());
+            verifyNoInteractions(kafka);
         }
 
         @Test
-        public void registration_failed_tooManyIdentifierAttempts(){
+        public void registration_failed_tooManyIdentifierAttempts() {
             when(userRepository.existsByUserIdentifier(anyString())).thenReturn(true);
             when(userIdentifier.generate()).thenReturn("USER");
 
-            assertThrows(UserIdentifierException.class, () -> {
-                registration.registration(request);
-            });
+            assertThrows(UserIdentifierException.class, () ->
+                    registration.registration(request));
 
             verify(userIdentifier, atLeast(10)).generate();
-
             verify(userRepository, never()).save(any());
+            verifyNoInteractions(kafka);
         }
-
     }
 }
